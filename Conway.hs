8387ef3b846f7c14c654766a1d4b49c437baab8b
Michel Rijnders <g.a.c.rijnders@gmail.com>
2012-09-10 23:01:56 +0200

    Add IO and make the grid "infinite"

--- Conway.hs
+++ Conway.hs
@@ -1,20 +1,35 @@
 module Conway where
 
-type Width  = Int
-type Height = Int
+import Data.List (intercalate)
+
 type Cell   = (Int,Int)
-type Grid   = (Width,Height,[Cell])
 
-nbCount :: Cell -> [Cell] -> Int
-nbCount (x,y) cs =
-    length (filter (\c -> c `elem` cs) 
-                   [(x+x',y+y') | x' <- [-1..1] 
-                                , y' <- [-1..1]
-                                , x' /= 0 || y' /= 0])
 
-next :: Grid -> Grid
-next (w,h,cs) = (w,h,cs')
-    where cs' = [(x,y) | x <- [1..w]
-                       , y <- [1..h]
-                       , ((x,y) `elem` cs && nbCount (x,y) cs == 2)
-                         || nbCount (x,y) cs == 3]
+putGrid :: [Cell] -> IO ()
+putGrid = putStrLn . toString
+
+next :: [Cell] -> [Cell]
+next cs = cs'
+    where
+      cs' = [(x,y) | x <- [(left cs)..(right cs)]
+                   , y <- [(top cs)..(bottom cs)]
+                   , ((x,y) `elem` cs && count (x,y) == 2)
+                     || count (x,y)  == 3]
+      count (x,y) = length (filter (\c -> c `elem` cs)
+                                   [(x+x',y+y') | x' <- [-1..1]
+                                                , y' <- [-1..1]
+                                                , x' /= 0 || y' /= 0])
+
+toString :: [Cell] -> String
+toString cs =
+    intercalate "\n"
+                (map (\y -> concatMap (\x -> render(x,y)) xs) ys)
+    where
+      xs = [(left cs)..(right cs)]
+      ys = [(top cs)..(bottom cs)]
+      render (x,y) = if (x,y) `elem` cs then "#" else "."
+
+left   cs = minimum (map fst cs) - 1
+right  cs = maximum (map fst cs) + 1
+top    cs = minimum (map snd cs) - 1
+bottom cs = maximum (map snd cs) + 1
