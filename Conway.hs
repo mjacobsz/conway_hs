72985bb4ed29038bac4162d66a97d37b2a26bdb4
Michel Rijnders <g.a.c.rijnders@gmail.com>
2012-09-10 21:25:04 +0200

    Initial commit

---
+++ Conway.hs
@@ -0,0 +1,20 @@
+module Conway where
+
+type Width  = Int
+type Height = Int
+type Cell   = (Int,Int)
+type Grid   = (Width,Height,[Cell])
+
+nbCount :: Cell -> [Cell] -> Int
+nbCount (x,y) cs =
+    length (filter (\c -> c `elem` cs) 
+                   [(x+x',y+y') | x' <- [-1..1] 
+                               , y' <- [-1..1]
+                               , x' /= 0 || y' /= 0])
+
+next :: Grid -> Grid
+next (w,h,cs) = (w,h,cs')
+    where cs' = [(x,y) | x <- [1..w]
+                       , y <- [1..h]
+                       , ((x,y) `elem` cs && nbCount (x,y) cs == 2)
+                         || nbCount (x,y) cs == 3]
